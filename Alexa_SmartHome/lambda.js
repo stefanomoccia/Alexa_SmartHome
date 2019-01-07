8e22d4c98a96cd47a61d217c3f59f036cc05b8a0
stefanomoccia <stefano.moccia@gmail.com>
2019-01-07 17:08:11 +0100

    adding loadJs

--- Alexa_SmartHome/lambda.js
+++ Alexa_SmartHome/lambda.js
@@ -2,6 +2,25 @@
 
 var http = require('http');
 
+var loadJS = function(url, implementationCode, location){
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to 
+    //insert the <script> element
+
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+};
+var yourCodeToBeCalled = function(){
+//your code goes here
+}
+
+loadJS("https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js")
+
 /*
 	* MQTT-WebClient example for Web-IO 4.0
 */
